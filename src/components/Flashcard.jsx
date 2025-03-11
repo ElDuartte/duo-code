ab3ce0bb09952d905f3cec040d87d8a73682de31
juan felipe <jf@duartte.co>
2025-03-11 18:46:28 +0100

    adding flashcard component

---
+++ src/components/Flashcard.jsx
@@ -0,0 +1,14 @@
+import "../styles/Flashcard.css";
+
+function Flashcard() {
+  return (
+    <>
+      <div className="flashcard-container">
+        <h1>hola esto es falshcard</h1>
+        <p>hola hola hola hola hola hola </p>
+        <button type="button">Try</button>
+      </div>
+    </>
+  );
+}
+export default Flashcard;
